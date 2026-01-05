fdb9786e05214daccc195094cd226220bb19b4d5
Shawn Gestupa <thereal.nwahs@gmail.com>
2026-01-05 16:31:53 +0800

    fix: enable & start docker service before running the container

--- scripts/ec2_init.sh
+++ scripts/ec2_init.sh
@@ -3,6 +3,9 @@
 yum update -y
 yum install -y docker
 
+systemctl enable docker
+systemctl start docker
+
 # The EC2 instance will be using t4g.nano, with an arm64 architecture;
 # It is best to have a container that can be ran on arm64 or simply build
 # an existing one with `docker build --platform linux/arm64 <username>/<image>:arm64`
