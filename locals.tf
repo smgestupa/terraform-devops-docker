2a5d922a758f302e405031617ecf5e300d3bd846
Shawn Gestupa <thereal.nwahs@gmail.com>
2026-01-05 14:43:57 +0800

    feat: use the correct CIDR block for allowed ingress rule

--- locals.tf
+++ locals.tf
@@ -1,7 +1,7 @@
 locals {
   create_ingress_rules = [
     for rule in merge(var.var.sg_app_create_ingress_rules, {
-      cidr_ipv4   = var.my_ip
+      cidr_ipv4   = "${var.my_ip}/32"
       from_port   = 80
       to_port     = 80
       ip_protocol = "tcp"
